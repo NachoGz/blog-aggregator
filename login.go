9ce8bc7a2e1f160a94acd6d190b494fc667355b2
NachoGz <ignaciogomezf1@gmail.com>
2025-02-16 20:19:52 -0300

    add GetUser before setting the user to verify that the user exists

--- login.go
+++ login.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"log"
 )
 
 func handleLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return errors.New("there are no arguments, one is expected")
 	}
-	err := s.cfg.SetUser(cmd.args[0])
+
+	user, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		log.Println("error fetching user")
+		return err
+	}
+
+	err = s.cfg.SetUser(user.Name)
 	if err != nil {
 		fmt.Printf("error setting user: %v", err)
 		return err
 	}
 
-	fmt.Printf("username %s has been set\n", cmd.args[0])
+	fmt.Printf("username %s has been set\n", user.Name)
 	return nil
 }
