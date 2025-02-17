046f4c6a57d62631d188cc4fb28c784ef9e3f37e
NachoGz <ignaciogomezf1@gmail.com>
2025-02-17 19:00:48 -0300

    add queries for getting a feed given a url, the same but with id. Also a query to mark a feeds as fetched with the time of fetching and then one for getting the next feed to fetch

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -14,3 +14,24 @@ RETURNING *;
 
 -- name: GetFeeds :many
 SELECT name, url, user_id FROM feeds;
+
+-- name: GetFeedFromURL :one
+SELECT * from feeds
+WHERE url = $1;
+
+-- name: GetFeedFromID :one
+SELECT * from feeds
+WHERE id = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET
+	updated_at = NOW(),
+	last_fetched_at = NOW()
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT *
+	FROM feeds
+	ORDER BY last_fetched_at ASC NULLS FIRST
+	LIMIT 1;
