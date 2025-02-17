17e4ce4c0dd188c9f3fbf1a17858dfdd8a1f7ec5
NachoGz <ignaciogomezf1@gmail.com>
2025-02-17 19:13:52 -0300

    add calls for handlers given the argument passed, implemented the middleware for those handlers that require user login. Lastly, i added a help command that calls the printUsage function that prints how gator is used and what commands are there and what do they do

--- main.go
+++ main.go
@@ -65,6 +65,8 @@ func main() {
 		args: cliArgs[2:],
 	}
 
+	run := true
+
 	switch cmd.name {
 	case "login":
 		cmds.register(cmd.name, handleLogin)
@@ -77,14 +79,51 @@ func main() {
 	case "agg":
 		cmds.register(cmd.name, handleAgg)
 	case "addfeed":
-		cmds.register(cmd.name, handleAddFeed)
+		cmds.register(cmd.name, middlewareLoggedIn(handleAddFeed))
 	case "feeds":
 		cmds.register(cmd.name, handlerFeeds)
+	case "follow":
+		cmds.register(cmd.name, middlewareLoggedIn(handleFollow))
+	case "following":
+		cmds.register(cmd.name, middlewareLoggedIn(handleFollowing))
+	case "unfollow":
+		cmds.register(cmd.name, middlewareLoggedIn(handleUnfollow))
+	case "browse":
+		cmds.register(cmd.name, middlewareLoggedIn(handleBrowse))
+	case "help":
+		run = false
+		printUsage()
 	}
 
-	err = cmds.run(&s, cmd)
-	if err != nil {
-		fmt.Println("error running the command: ", err)
-		os.Exit(1)
+	if run {
+		err = cmds.run(&s, cmd)
+		if err != nil {
+			fmt.Println("error running the command: ", err)
+			fmt.Println("Run './gator help' for usage.")
+			os.Exit(1)
+		}
 	}
+
+}
+
+func printUsage() {
+	fmt.Println("\t\t\t\t\t🐊 Gator is an RSS feed aggregator in Go! 🐊")
+	fmt.Println()
+	fmt.Println("Usage:")
+	fmt.Println()
+	fmt.Println("\t./gator <command> [arguments]")
+	fmt.Println()
+	fmt.Println("The commands are:")
+	fmt.Println()
+	fmt.Println("\t login	\t sets the current user, usage: ./gator login <username>")
+	fmt.Println("\t register\t adds a new user to the database, usage: ./gator register <username>")
+	fmt.Println("\t users	\t lists all the users in the database, usage: ./gator users")
+	fmt.Println("\t reset	\t resets the state of the database, i.e deletes all users and records associated to them. Usage: ./gator reset")
+	fmt.Println("\t agg	\t fetches the RSS feeds, parse them and stores the as posts in the database. Usage: ./gator agg <time-between-reqs>")
+	fmt.Println("\t addfeed\t adds feeds to the database, usage: ./gator addfeed <feed-name> <feed-url>")
+	fmt.Println("\t feeds	\t lists all the feeds in the database, usage: ./gator feeds")
+	fmt.Println("\t follow	\t the current user starts to follow the given feed, usage: ./gator follow <feed-url>")
+	fmt.Println("\t following\t lists all the feeds that the current user is following, usage: ./gator following")
+	fmt.Println("\t unfollow\t the current users unfollows the given feed, usage: ./gator unfollow <feed-url>")
+	fmt.Println("\t browse	\t view all the posts from the feeds the user follows, usage: ./gator browse <limit> (default is 2)")
 }
