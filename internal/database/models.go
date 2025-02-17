bde30724170373d8daf8cf913bd9dcf919d6e20d
NachoGz <ignaciogomezf1@gmail.com>
2025-02-17 19:03:45 -0300

    go structs for all the db tables

--- internal/database/models.go
+++ internal/database/models.go
@@ -5,18 +5,39 @@
 package database
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 type Feed struct {
+	ID            uuid.UUID
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Name          string
+	Url           string
+	UserID        uuid.NullUUID
+	LastFetchedAt sql.NullTime
+}
+
+type FeedFollow struct {
 	ID        uuid.UUID
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	Name      string
-	Url       string
 	UserID    uuid.NullUUID
+	FeedID    uuid.NullUUID
+}
+
+type Post struct {
+	ID          uuid.UUID
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	Title       string
+	Url         string
+	Description string
+	PublishedAt time.Time
+	FeedID      uuid.NullUUID
 }
 
 type User struct {
